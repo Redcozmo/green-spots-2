35a18c803161e24a40f1f58f620075d78781b340
redcozmo <redcozmo@hotmail.com>
2019-01-31 16:41:34 +0100

    add taxa count method on a spot

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,4 +1,17 @@
 class Spot < ApplicationRecord
   has_many :trees
   # belongs_to :user
+
+  # Methods
+  # count the different number of taxa of a spot
+  def taxa_count()
+    # list of taxa of each tree of the spot
+    taxa_tab = []
+    trees.each do |tree|
+      taxa_tab << tree.taxon.common_noun
+    end
+    # calculation of the total taxa without duplication
+    tot = taxa_tab.uniq.length
+    return tot
+  end
 end
