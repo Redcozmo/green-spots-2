c3141d3f07c4c54847b3a05299bb68ef7a8a17b2
redcozmo <redcozmo@hotmail.com>
2019-02-01 15:12:25 +0100

    create spot show page

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -2,7 +2,12 @@ class SpotsController < ApplicationController
   def new
     @spot = Spot.new
   end
+
   def index
     @spots = Spot.all
+  end
+
+  def show
+    @spot = Spot.find(params[:id])
   end
 end
