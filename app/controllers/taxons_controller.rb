67d5833d97db2fe6efd9ec7f084b27ea9f466b7f
redcozmo <redcozmo@hotmail.com>
2019-03-15 00:24:37 +0100

    new-create-show taxon

--- app/controllers/taxons_controller.rb
+++ app/controllers/taxons_controller.rb
@@ -1,11 +1,32 @@
 class TaxonsController < ApplicationController
-  
+
   def new
     @taxon = Taxon.new
   end
 
   def index
     @taxons = Taxon.all
+  end
+
+  def create
+    taxon = Taxon.new(post_params)
+    if taxon.valid?
+      taxon.save
+      redirect_to taxon_path(taxon.id), success: "Taxon créé avec succès"
+    else
+      @taxon = taxon
+      render 'new'
+    end
+  end
+
+  def show
+    @taxon = Taxon.find(params[:id])
+  end
+
+  private
+
+  def post_params
+    params.require(:taxon).permit(:common_noun, :family, :genus, :species)
   end
 
 end
