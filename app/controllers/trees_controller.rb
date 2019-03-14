b4cb74da6e3ce3c7bb7a384bdac7b95c280bb110
redcozmo <redcozmo@hotmail.com>
2019-03-14 23:59:47 +0100

    new-create-show tree

--- app/controllers/trees_controller.rb
+++ app/controllers/trees_controller.rb
@@ -8,8 +8,29 @@ class TreesController < ApplicationController
     @trees = Tree.all
   end
 
+  def create
+    tree = Tree.new(post_params)
+    if tree.valid?
+      tree.save
+      redirect_to tree_path(tree.id), success: "Arbre créé avec succès"
+    else
+      @tree = tree
+      render 'new'
+    end
+  end
+
+  def show
+    @tree = Tree.find(params[:id])
+  end
+
   def map
     @trees = Tree.all
+  end
+
+  private
+
+  def post_params
+    params.require(:tree).permit(:name, :description, :latitude, :longitude, :taxon_id, :spot_id)
   end
 
 end
